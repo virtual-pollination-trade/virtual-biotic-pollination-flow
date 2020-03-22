ebcb5d456269df44e328993cbcc125538c47c462
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-03-22 17:10:38 -0300

    fix server structure allowing to used the plot in another output$*
    Closes #3

--- app.R
+++ app.R
@@ -2,16 +2,16 @@ source("./global.R")
 
 ui <- fluidPage(
 
-  # path to google analytics
+  # path google analytics
   tags$head(includeHTML(("www/google-analytics.html"))),
 
   # path css file
-  theme = "style.css",
+  # theme = "style.css",
 
   # Application title
   titlePanel("Virtual Biotic Pollination Flow"),
 
-  # Sidebar with a slider input for number of bins
+  # Sidebar application menu
   sidebarLayout(
 
     sidebarPanel(
@@ -51,11 +51,13 @@ ui <- fluidPage(
         selectize = TRUE
       ),
 
-      actionButton("make_plot", "Create map")
+      actionButton(inputId = "make_plot", label = "Reload map", icon("sync-alt")),
+      
+      downloadButton(outputId = "download_map", label = "Download map")
 
     ),
 
-    # Show a plot of the generated distribution
+    # Show map
     mainPanel(
 
       plotOutput(outputId = "map", height = "700px", width = "100%"),
@@ -70,77 +72,86 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   message("\n\nLoading server ...\n\n")
-
-  # TODO: Create reactive objects 
-  # BODY: The filtered data need to be reactive objects to be used into differents
-  #       parts of output.
-  #       see: https://stackoverflow.com/a/19466838
   
-  output$map <- renderPlot({
-
+  virtual_pollinators_flow_filtered <- reactive({
+    
+    virtual_pollinators_flow %>% 
+      filter_countries_by_input_select_countries(
+        input_origin = input$origin, 
+        input_destination = input$destination
+      ) %>%
+      filter_countries_by_input_select_year(input_year = input$year)
+    
+  })
+  
+  vp_flow_year <- reactive({
+    
+    virtual_pollinators_flow %>%
+      filter_year_by_input_select_year(input$year)
+    
+  })
+  
+  plotInput <- reactive({
+    
     input$make_plot
+    
+    data_filtered <- virtual_pollinators_flow_filtered()
+    data_sf <- country_features_with_sf_geometry
+    data_year <- vp_flow_year()
+    
+    make_plot_by_input_colormap(
+      data_filtered = data_filtered,
+      data_sf = data_sf,
+      data_year = data_year,
+      input_colormap = input$colormap,
+      input_origin = input$origin,
+      input_destination = input$destination
+    )
+    
+  })
+      
+  output$map <- renderPlot({
 
     req(input$origin)
     req(input$destination)
     
-    message("Checking input variables ...\n\n")
-    
-    print(input$origin)
-    print(input$destination)
-    print(input$year)
-    print(input$colormap)
-
     withProgress(
 
       message = "Creating map ...", {
+        
+      message("Checking input variables ...\n\n")
       
+      print(input$origin)
+      print(input$destination)
+      print(input$year)
+      print(input$colormap)
+
       message("\n\nFiltering countries in virtual_pollinators_flow...\n\n")
 
-      virtual_pollinators_flow_filtered <-
-        virtual_pollinators_flow %>% 
-        filter_countries_by_input_select_countries(
-          input_origin = input$origin, 
-          input_destination = input$destination
-        ) %>%
-        filter_countries_by_input_select_year(input_year = input$year)
-      
       message("Checking lowest `vp_flow` values ...\n\n")
-      
-      virtual_pollinators_flow_filtered %>%
-        select(reporter_countries, partner_countries, vp_flow) %>% 
-        head() %>% 
+
+      virtual_pollinators_flow_filtered() %>%
+        select(reporter_countries, partner_countries, vp_flow) %>%
+        head() %>%
         print()
-      
+
       message("\n\nChecking highest `vp_flow` values ...\n\n")
-      
-      virtual_pollinators_flow_filtered %>%
-        select(reporter_countries, partner_countries, vp_flow) %>% 
-        tail() %>% 
-        print()
-      
-      message("\n\nFiltering years ...\n\n")
-      
-      vp_flow_year <- 
-        virtual_pollinators_flow %>% 
-        filter_year_by_input_select_year(input$year)
 
-      message("Checking filtered year(s) ...\n\n")
-      
-      vp_flow_year %>% 
+      virtual_pollinators_flow_filtered() %>%
+        select(reporter_countries, partner_countries, vp_flow) %>%
+        tail() %>%
         print()
 
+      message("\n\nChecking filtered year(s) ...\n\n")
+
+      vp_flow_year() %>%
+        print()
+      
       cat("\n\n")
       usethis::ui_todo("Creating map ...\n\n")
 
-      make_plot_by_input_colormap(
-        data_filtered = virtual_pollinators_flow_filtered,
-        data_sf = country_features_with_sf_geometry,
-        data_year = vp_flow_year,
-        input_colormap = input$colormap,
-        input_origin = input$origin,
-        input_destination = input$destination 
-      )
-
+      plotInput()
+      
     })
     
     cat("\n")
@@ -152,6 +163,19 @@ server <- function(input, output) {
   #   renderText({
   #     
   #   })
+  
+  output$download_map <- downloadHandler(
+    filename = "flow_map.png",
+    content = function(file) {
+      
+      usethis::ui_todo("Saving map ...\n\n")
+      
+      ggsave(filename = file, plot = plotInput())
+      
+      cat("\n")
+      usethis::ui_done("Map saved!\n\n")
+      
+    })
 
 }
 
