0ac02e3b9a153d5741d926d7a95ace290871024f
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-07-15 10:27:51 -0300

    remove old shiny tests

--- tests/test-shinyapp-datatable.R
+++
@@ -1,8 +0,0 @@
-app <- ShinyDriver$new("../")
-app$snapshotInit("test-shinyapp-datatable")
-
-app$setInputs(year = "2001")
-app$setInputs(colormap = "HDI")
-app$setInputs(destination = character(0))
-app$setInputs(destination = "Brazil")
-app$snapshot()
