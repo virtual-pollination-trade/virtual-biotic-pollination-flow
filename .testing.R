ecc5360eb4416ee8294c96283ea3d3e5e594d11d
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-07-16 20:10:47 -0300

    update auxiliar script

--- .testing.R
+++ .testing.R
@@ -1,7 +1,7 @@
 source("./global.R", local = TRUE)
 
 input <- list()
-input$origin <- "United States of America"
+input$origin <- c("United States of America", "China", "Brazil")
 input$destination <- "All countries"
 input$year <- "All years"
 input$colormap <- "HDI"
@@ -14,10 +14,10 @@ input$colormap <- "HDI"
 
 message("Checking input variables ...\n\n")
 
-print(input$origin)
-print(input$destination)
-print(input$year)
-print(input$colormap)
+ui_info("origin: {ui_code(input$origin)}")
+ui_info("destination: {ui_code(input$destination)}")
+ui_info("year: {ui_code(input$year)}")
+ui_info("colormap: {ui_code(input$colormap)}")
 
 message("\n\nFiltering countries in virtual_pollinators_flow...\n\n")
 
@@ -68,3 +68,23 @@ make_plot_by_input_colormap(
 
 cat("\n")
 ui_done("Map done!\n\n")
+
+############################################################
+#                                                          #
+#              fixing test after update data               #
+#                                                          #
+############################################################
+# devtools::load_all()
+
+# test_file("tests/testthat/test-distinct_countries_for_dt.R")
+# test_file("tests/testthat/test-distinct_countries.R")
+# test_file("tests/testthat/test-distinct_input_select_countries.R")
+# test_file("tests/testthat/test-distinct_input_select_years.R")
+# test_file("tests/testthat/test-filter_countries_by_input_select_countries.R")
+# test_file("tests/testthat/test-filter_countries_by_input_select_year.R")
+# test_file("tests/testthat/test-filter_year_by_input_select_year.R")
+# test_file("tests/testthat/test-min_max_vp_flow_by_input_year.R")
+# test_file("tests/testthat/test-no_vp_flow.R")
+# test_file("tests/testthat/test-sf_data_content.R")
+# test_file("tests/testthat/test-summarise_vp_flow_all_years.R")
+# test_file("tests/testthat/test-vp_flow_data_content.R")
